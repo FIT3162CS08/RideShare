31041089eb612067de9c5c84ac78abf6320b4f4e
Eric <erictheov@hotmail.com>
2025-08-30 18:26:30 +1000

    -Added text items for the homepage - Made the navbar gray temporarily for visibility

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <header className="w-full border-b bg-white">
+    <header className="w-full border-b bg-gray-300">
       <div className="mx-auto flex max-w-6xl items-center justify-between px-4 py-8">
         
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,12 @@ import Image from "next/image";
 
 export default function HomePage() {
   return (
-    <div></div>
+    <section>
+    <div className = "px-50">Travel to campus with RideShare</div>
+    <div className = "px-50">Log in to see your account details</div>
+    <div className = "px-50">Request rides, view past trips, tailored suggestions and more.</div>
+    <button className = "px-4 py-2 rounded-lg bg-black text-white hover:bg-gray-800">Log in to your account</button>
+    <div className = "px-50">Create account</div>
+    </section>
   );
 }
