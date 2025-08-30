a033e7f749ebdc6dee610eec790b2594efcf0b18
Eric <erictheov@hotmail.com>
2025-08-30 18:53:24 +1000

    - Added styling to the text items - Removed skeleton images and added image for landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,22 @@ import Image from "next/image";
 
 export default function HomePage() {
   return (
-    <section>
-    <div className = "px-50">Travel to campus with RideShare</div>
-    <div className = "px-50">Log in to see your account details</div>
-    <div className = "px-50">Request rides, view past trips, tailored suggestions and more.</div>
-    <button className = "px-4 py-2 rounded-lg bg-black text-white hover:bg-gray-800">Log in to your account</button>
-    <div className = "px-50">Create account</div>
+    <section className = "px-8 md:px-20 lg:px-40 py-12">
+    <div className = "space-y-4">
+      <h1 className = "text-4xl font-bold">Travel to campus with RideShare</h1>
+      <Image 
+        src="/homepage.png"      
+        alt="Uni Students illustration"
+        width={500} 
+        height={300} 
+        className="rounded-lg "
+      />
+
+      <p className = "text-3xl"> Log in to see your account details</p>
+      <p>Request rides, view past trips, tailored suggestions and more.</p>
+      <button className = "text-xl px-4 py-2 rounded-lg bg-black text-white hover:bg-gray-800">Log in to your account</button>
+      <p className="underline">Create account</p> 
+      </div>
     </section>
   );
 }
