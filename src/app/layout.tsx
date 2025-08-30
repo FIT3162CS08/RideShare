33b5a40e9d121f3fc71baa77bf6d3cd890b80db4
Eric <erictheov@hotmail.com>
2025-08-30 19:54:54 +1000

    - Added second layout of home page when the user logs in - Fixed navbar placement

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,10 +15,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Navbar />
+      
       <body
         className={`${roboto} antialiased`}
       >
+        <Navbar />
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,59 @@
 import Image from "next/image";
 
 export default function HomePage() {
-  return (
-    <section className = "px-8 md:px-20 lg:px-40 py-12">
-    <div className = "space-y-4">
-      <h1 className = "text-4xl font-bold">Travel to campus with RideShare</h1>
-      <Image 
-        src="/homepage.png"      
-        alt="Uni Students illustration"
-        width={500} 
-        height={300} 
-        className="rounded-lg "
-      />
+  const isLoggedIn = true;
 
-      <p className = "text-3xl"> Log in to see your account details</p>
-      <p>Request rides, view past trips, tailored suggestions and more.</p>
-      <button className = "text-xl px-4 py-2 rounded-lg bg-black text-white hover:bg-gray-800">Log in to your account</button>
-      <p className="underline">Create account</p> 
-      </div>
-    </section>
-  );
+  if (!isLoggedIn) {
+    return (
+      <section className="px-8 md:px-20 lg:px-40 py-12">
+        <div className="space-y-4">
+          <h1 className="text-4xl font-bold">
+            Travel to campus with RideShare
+          </h1>
+          <Image
+            src="/homepage.png"
+            alt="Uni Students illustration"
+            width={500}
+            height={300}
+            className="rounded-lg "
+          />
+
+          <p className="text-3xl"> Log in to see your account details</p>
+          <p>Request rides, view past trips, tailored suggestions and more.</p>
+          <button className="text-xl px-4 py-2 rounded-lg bg-black text-white hover:bg-gray-800">
+            Log in to your account
+          </button>
+          <p className="underline">Create account</p>
+        </div>
+      </section>
+    );
+  }
+
+  return <section className="px-8 md:px-20 lg:px-40 py-12">
+    <div className="space-y-4">
+        <h1 className="text-4xl font-bold">
+          Travel to campus with RideShare
+        </h1>
+
+      <div className="relative">
+        <span> </span>
+        <input
+          type="text"
+          placeholder="Pick-up location"
+          className="w-3/4 rounded-lg border px-3 py-2 text-sm outline-none focus:ring-2 focus:ring-black/20"
+        />
+        </div>
+        <input
+          type="text"
+          placeholder="Campus"
+          className="w-3/4 rounded-lg border px-3 py-2 text-sm outline-none focus:ring-2 focus:ring-black/20"
+        />
+        <input
+          type="text"
+          placeholder="Date & time"
+          className="w-3/4 rounded-lg border px-3 py-2 text-sm outline-none focus:ring-2 focus:ring-black/20"
+        />
+
+    </div>
+  </section>;
 }
