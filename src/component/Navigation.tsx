1d00f518180e073a8e37d500b1b26ae4693e5c49
Jensen <jkut0001@student.monash.edu>
2025-10-03 20:07:40 +1000

    removing redundant navlink

--- src/component/Navigation.tsx
+++ src/component/Navigation.tsx
@@ -1,32 +1,29 @@
-"use client";
+import React from 'react'
+import Link from 'next/link'
 
-import React from "react";
-import NavLink from "./navigation/NavLink";
-
-const img_url = "https://cdn-icons-png.flaticon.com/512/1571/1571921.png";
+const img_url = "https://cdn-icons-png.flaticon.com/512/1571/1571921.png"
 
 const Navigation = () => {
   return (
-    <nav className="px-20 bg-gray-900 text-white flex gap-6 items-center justify-between h-20 font-mono">
-      <div className='flex items-center justify-center gap-16'>
-        <NavLink href="/">
-          <img src={img_url} className='h-15'/>
-        </NavLink>
-        <div className='flex gap-12'>
-          <NavLink href="/">△ Home</NavLink>
-          <NavLink href="/booking">△ Book Ride</NavLink>
-          <NavLink href="/trip">△ My Trip</NavLink>
-          <NavLink href="/driver_portal">△ Drive</NavLink>
-        </div>
-      </div>
-      <div className='flex items-center gap-14'>
-        <NavLink href="/user">Login</NavLink>
-        <NavLink href="/about">Sign Up</NavLink>
-      </div>
-    </nav>
-  );
+        <nav className="px-20 bg-gray-900 text-white flex gap-6 items-center justify-between h-20 font-mono">
+            <div className='flex items-center justify-center gap-16'>
+                <Link href="/">
+                    <img src={img_url} className='h-15'/>
+                </Link>
+                <div className='flex gap-12'>
+                    <Link href="/">△ Home</Link>
+                    <Link href="/booking">△ Book Ride</Link>
+                    <Link href="/trip">△ My Trip</Link>
+                    <Link href="/driver_portal">△ Drive</Link>
+                    <Link href="/settings">△ Settings</Link>
+                </div>
+            </div>
+            <div className='flex items-center gap-14'>
+                <Link href="/login">Login</Link>
+                <Link href="/signup">Sign Up</Link>
+            </div>
+        </nav>
+  )
 }
 
-export default Navigation;
-
-
+export default Navigation
